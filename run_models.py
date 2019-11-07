4adadfea96c35586a6330d98bcf59d3f14d5a723
Lott <TL05684@de.corp.danet.com>
2019-11-07 18:36:39 +0100

    Tobi; some fixes

--- run_models.py
+++ run_models.py
@@ -28,13 +28,16 @@ def main():
     project_id = "nemo-life-assistant"
     compute_region = "us-central1"
     model_id = "heartrates_v1"
-    file_path = "./hr_model/sample_data.csv"
-    score_threshold = 0.5
+    file_path = "./hr_model/sample_data.CSV"
+    score_threshold = "0.5"
+    model_display_name = "heartrates_v1"
 
     # Get full model path and create prediction client for AutoML Tables
+    # REQUIRES CREDENTIALS AND AUTHENTICATION (OAuth)
     automl_client = automl.AutoMlClient()
     model_full_id = automl_client.model_path(project_id, compute_region, model_id)
-    prediction_client = automl.PredictionServiceClient()
+    #prediction_client = automl.PredictionServiceClient()
+    client = automl.TablesClient(project=project_id, region=compute_region)
 
     # Initialize params
     params = {}
@@ -45,11 +48,11 @@ def main():
     modus = "fall"
     count = 0
     while modus == "fall":
-        count += 1
+        count += 5
 
         # Take picture
         path = "./pictures"
-        subprocess.run("cd {} && snapshot --oneshot --prefix face".format(path))
+        subprocess.run("cd {} && snapshot --oneshot --prefix face".format(path), shell=True)
         print("----------took picture----------")
         # Open image
         paths = []
@@ -67,7 +70,7 @@ def main():
             print("score = ", ans.score)
         else:
             print("----------no face detected----------")
-            playsound("./sounds/no_nemo.mp3")
+            #playsound("/audio/no_nemo.mp3")
 
         if count%10 == 0:
             modus = "hr"
@@ -81,9 +84,16 @@ def main():
         row = int(str(count)[-2:-1])
         with open(file_path, "rt") as csv_file:
             values = [float(i) for i in list(csv.reader(csv_file))[row]]
-            request = {"payload": {"row": {"values": values}}}
+            #request = {"payload": {"row": {"values": values}}}
+            inputs= {'male': str(values[0]),
+                    'heartRate': values[1],
+                    'age':values[2]
+            }
+
+        print(inputs)
         # Query AutoML Tables model
-        response = prediction_client.predict(model_full_id, request, params)
+        #response = prediction_client.predict(model_full_id, payload, params)
+        response = client.predict(model_display_name=model_display_name, inputs=inputs)
         print("----------analyze heartrate----------")
         for result in response.payload:
         # laut Doku result.display_name; nicht sicher was display_name ist    
@@ -96,7 +106,7 @@ def main():
                 playsound("./sounds/bad_hr.mp3")
             else:
                 print("not able to analyze heartrate")
-        # Return to fall detection mode
+        #Return to fall detection mode
         modus = "fall"
 
 if __name__ == '__main__':
