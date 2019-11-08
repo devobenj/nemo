e67c122b2af3841c61a8644c449ae6d820189bc6
Lott <TL05684@de.corp.danet.com>
2019-11-08 11:23:43 +0100

    status: runs on board

--- run_models.py
+++ run_models.py
@@ -6,6 +6,7 @@ from PIL import Image
 from google.cloud import automl_v1beta1 as automl
 import csv
 from playsound import playsound
+from google.oauth2 import service_account
 
 # Function to read emotion labels from text files
 def ReadLabelFile(file_path):
@@ -30,84 +31,80 @@ def main():
     model_id = "heartrates_v1"
     file_path = "./hr_model/sample_data.CSV"
     score_threshold = "0.5"
-    model_display_name = "heartrates_v1"
 
-    # Get full model path and create prediction client for AutoML Tables
-    # REQUIRES CREDENTIALS AND AUTHENTICATION (OAuth)
-    automl_client = automl.AutoMlClient()
-    model_full_id = automl_client.model_path(project_id, compute_region, model_id)
-    #prediction_client = automl.PredictionServiceClient()
-    client = automl.TablesClient(project=project_id, region=compute_region)
+    # Create prediction client for AutoML Tables using service account credentials
+    client = automl.TablesClient(credentials=service_account.Credentials.from_service_account_file('nemo-hrml-key.json'),
+                                 project=project_id, region=compute_region)
 
     # Initialize params
     params = {}
     if score_threshold:
         params = {"score_threshold": score_threshold}
+    path = "./pictures"
 
     # Initialize variables
     modus = "fall"
     count = 0
-    while modus == "fall":
-        count += 5
 
-        # Take picture
-        path = "./pictures"
-        subprocess.run("cd {} && snapshot --oneshot --prefix face".format(path), shell=True)
-        print("----------took picture----------")
-        # Open image
-        paths = []
-        for file in os.listdir(path):
-            paths.append(os.path.join(path, file))
-        img = Image.open(paths[-1])
-        print("----------opened image----------")
-        # Run inference
-        ans = emotion_engine.detect_with_image(img, threshold=0.5, keep_aspect_ratio=True, relative_coord=True, top_k=1)
+    while True:
+        while modus == "fall":
+            count += 1
+            # Take picture
+            subprocess.run("cd {} && snapshot --oneshot --prefix face".format(path), shell=True)
+            print("----------took picture----------")
+            # Open image
+            paths = []
+            for file in os.listdir(path):
+                paths.append(os.path.join(path, file))
+            img = Image.open(paths[-1])
+            print("----------opened image----------")
+            # Run inference
+            ans = emotion_engine.detect_with_image(img, threshold=0.5, keep_aspect_ratio=True, relative_coord=True, top_k=1)
 
-        if ans:
-            print("----------face detected----------")
-            current_emotion = emotion_labels[ans.label_id]
-            print("emotion = ", current_emotion)
-            print("score = ", ans.score)
-        else:
-            print("----------no face detected----------")
-            #playsound("/audio/no_nemo.mp3")
+            if ans:
+                print("----------face detected----------")
+                current_emotion = emotion_labels[ans.label_id]
+                print("emotion = ", current_emotion)
+                print("score = ", ans.score)
+            else:
+                print("----------no face detected----------")
+                #playsound("/audio/no_nemo.mp3")
 
-        if count%10 == 0:
-            modus = "hr"
+            if count%10 == 0:
+                modus = "hr"
 
-        # evtl. Bilder wieder aus Ordner löschen?
-        time.sleep(5)
+            # evtl. Bilder wieder aus Ordner löschen?
+            time.sleep(5)
 
-    while modus == "hr":
-        # Open CSV-file input
-        print("----------read heartrate----------")
-        row = int(str(count)[-2:-1])
-        with open(file_path, "rt") as csv_file:
-            values = [float(i) for i in list(csv.reader(csv_file))[row]]
-            #request = {"payload": {"row": {"values": values}}}
-            inputs= {'male': str(values[0]),
-                    'heartRate': values[1],
-                    'age':values[2]
-            }
+        while modus == "hr":
+            # Open CSV-file input
+            print("----------read heartrate----------")
+            row = int(str(count)[-2:-1])
+            with open(file_path, "rt") as csv_file:
+                values = [float(i) for i in list(csv.reader(csv_file))[row]]
+                #request = {"payload": {"row": {"values": values}}}
+                inputs= {'male': str(values[0]),
+                        'heartRate': values[1],
+                        'age':values[2]
+                }
 
-        print(inputs)
-        # Query AutoML Tables model
-        #response = prediction_client.predict(model_full_id, payload, params)
-        response = client.predict(model_display_name=model_display_name, inputs=inputs)
-        print("----------analyze heartrate----------")
-        for result in response.payload:
-        # laut Doku result.display_name; nicht sicher was display_name ist    
-            if result.ok == 1:
+            # Query AutoML Tables model
+            response = client.predict(model_display_name=model_id, inputs=inputs)
+            print("----------analyze heartrate----------")
+            result =  response.payload[0].tables if (response.payload[0].tables.score > response.payload[1].tables.score) else response.payload[1].tables
+            if(result.value.string_value == '1'):
                 print("heartrate is okay")
-                print("score = {}".format(result.classification.score))
-            elif result.ok == 0:
+                print("score = {}".format(result.score))
+            elif(result.value.string_value == '0'):
                 print("heartrate is not okay")
-                print("score = {}".format(result.classification.score))
+                print("score = {}".format(result.score))
                 playsound("./sounds/bad_hr.mp3")
             else:
                 print("not able to analyze heartrate")
-        #Return to fall detection mode
-        modus = "fall"
+
+            # #Return to fall detection mode
+            count = 0
+            modus = "fall"
 
 if __name__ == '__main__':
   main()
